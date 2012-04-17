9a567112674ecdaa4a669ad1913716de96884657
Steven Shingler <shingler@gmail.com>
2012-04-17 01:53:28 +0100

    Tired tidying

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -5,23 +5,19 @@ module CapistranoResque
   class CapistranoIntegration
     def self.load_into(capistrano_config)
       capistrano_config.load do
-        
+
         _cset(:num_of_queues, 2)
         _cset(:queue_name, "*")
         _cset(:app_env, (fetch(:rails_env) rescue 'production'))
 
-        # Check if remote file exists
-        #
         def remote_file_exists?(full_path)
           'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
         end
-        
-        # Check if process is running
-        #
+
         def remote_process_exists?(pid_file)
           capture("ps -p $(cat #{pid_file}) ; true").strip.split("\n").size == 2
         end
-        
+
         namespace :resque do
           desc "Restart running workers"
           task :restart_workers do
@@ -29,7 +25,7 @@ module CapistranoResque
             Rake::Task['resque:start_workers'].invoke
           end
 
-          desc "Quit running workers"
+          desc "Quit running Resque workers"
           task :stop_workers do
             num_of_queues.times do |i|
               pid = "#{current_path}/tmp/pids/resque_worker_#{i}.pid"
@@ -47,10 +43,9 @@ module CapistranoResque
             end
           end
 
-          desc "Start workers"
+          desc "Start Resque workers"
           task :start_workers do
             puts "Starting #{num_of_queues} worker(s) with QUEUE: #{queue_name}"
-            require "rubygems"; require "ruby-debug"; debugger
             num_of_queues.times do |i|
               pid = "./tmp/pids/resque_worker_#{i}.pid"
               run "cd #{current_path} && RAILS_ENV=#{app_env} QUEUE=#{queue_name} \
