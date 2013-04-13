d4b72f7e70d4ed8ba717413860d518b3a318ebf6
Dean Perry <dean@deanperry.net>
2013-04-14 00:19:09 +0200

    added environment

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -35,7 +35,7 @@ module CapistranoResque
         def start_command(queue, pid)
           "cd #{current_path} && RAILS_ENV=#{rails_env} QUEUE=\"#{queue}\" \
            PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 INTERVAL=#{interval} \
-           #{fetch(:bundle_cmd, "bundle")} exec rake resque:work"
+           #{fetch(:bundle_cmd, "bundle")} exec rake environment resque:work"
         end
 
         def stop_command
@@ -49,7 +49,7 @@ module CapistranoResque
         def start_scheduler(pid)
           "cd #{current_path} && RAILS_ENV=#{rails_env} \
            PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 \
-           #{fetch(:bundle_cmd, "bundle")} exec rake resque:scheduler"
+           #{fetch(:bundle_cmd, "bundle")} exec rake environment resque:scheduler"
         end
 
         def stop_scheduler(pid)
