1c8aa6de68ac127869f0dba409a77487b7e69cab
Dean Perry <dean@deanperry.net>
2013-04-14 20:06:48 +0200

    fixed queue

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -33,9 +33,7 @@ module CapistranoResque
         end
 
         def start_command(queue, pid)
-          "cd #{current_path} && RAILS_ENV=#{rails_env} QUEUE=\"#{queue}\" \
-           PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 INTERVAL=#{interval} \
-           #{fetch(:bundle_cmd, "bundle")} exec rake environment resque:work"
+          "cd #{current_path} && RAILS_ENV=#{rails_env} QUEUE='#{queue}' PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 INTERVAL=#{interval} #{fetch(:bundle_cmd, "bundle")} exec rake environment resque:work"
         end
 
         def stop_command
