154e0b45e6f2c7e7ce47acbb0d52fb4df0fd5659
Dylan Markow <dylan@dylanmarkow.com>
2013-10-18 21:17:14 -0500

    Load different set of tasks based on Capistrano's version

--- lib/capistrano-resque.rb
+++ lib/capistrano-resque.rb
@@ -1,2 +1,8 @@
 require "capistrano-resque/version"
-require "capistrano-resque/capistrano_integration"
+
+if Gem::Version.new(Capistrano::VERSION) >= Gem::Version.new("3.0.0")
+  load File.expand_path("../capistrano-resque/tasks/capistrano-resque.rake", __FILE__)
+else
+  require "capistrano-resque/capistrano_integration"
+end
+
