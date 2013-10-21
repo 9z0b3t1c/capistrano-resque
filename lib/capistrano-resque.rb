bab99cefa0b3783f5ee29d1b5b28c16a985bf24b
Dylan Markow <dylan@dylanmarkow.com>
2013-10-21 13:19:10 -0500

    Compare the release version of Capistrano to accommodate pre-release versions

--- lib/capistrano-resque.rb
+++ lib/capistrano-resque.rb
@@ -1,6 +1,6 @@
 require "capistrano-resque/version"
 
-if Gem::Version.new(Capistrano::VERSION) >= Gem::Version.new("3.0.0")
+if Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new("3.0.0")
   load File.expand_path("../capistrano-resque/tasks/capistrano-resque.rake", __FILE__)
 else
   require "capistrano-resque/capistrano_integration"
