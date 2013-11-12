e7254585671e668d65fdb3007710b7de352b7745
Dylan Markow <dylan@dylanmarkow.com>
2013-11-12 10:02:17 -0600

    Check if Capistrano::VERSION is defined
    V2 uses Capistrano::Version, not ::VERSION, so this would have thrown
    an error when used in V2. (Thanks to @avaranovich for pointing this out)

--- lib/capistrano-resque.rb
+++ lib/capistrano-resque.rb
@@ -1,6 +1,6 @@
 require "capistrano-resque/version"
 
-if Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new("3.0.0")
+if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new("3.0.0")
   load File.expand_path("../capistrano-resque/tasks/capistrano-resque.rake", __FILE__)
 else
   require "capistrano-resque/capistrano_integration"
